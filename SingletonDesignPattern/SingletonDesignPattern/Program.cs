feb45984bd97e5d7e4b5a58db48b5f8e3fea73ad
tubaM <46338052+tubaM@users.noreply.github.com>
2019-07-04 23:08:44 +0530

    Add files via upload

---
+++ SingletonDesignPattern/SingletonDesignPattern/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton.Instance.Print();
+        }
+    }
+
+    public sealed class Singleton
+    {
+        private static Singleton instance = null;
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton Instance {
+            get {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Singleton instance called");
+        }
+    }
+}
+
